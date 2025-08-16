c8a1974e2b7b7d00deb666f6dd88e74d46aa87f1
Adinathtambe <adinathtambe4@gmail.com>
2025-08-16 13:48:34 +0530

    Create demo.c

---
+++ demo.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+void main(){
+	//minutes and hours
+//	int min =230;
+//	int hr = 230/60;
+//	int rmin = 230%60;
+//printf("%d minutes is equal to %d hour and %d minutes",min,hr,rmin);
+
+
+    //celsius to fahrenheit
+//double c,f ;
+//c=50;
+//f = c*9/5 +32 ;
+//printf("%f",f);
+
+
+   //addition of the no.
+   
+//   int no = 456;
+//   int n1 = no/10;
+//   int n2 = no%10;
+//   int n3 = n1/10;
+//   int n4 = n1%10;
+//   
+//   int sum = n2+n3+n4;
+//   
+//   
+//   printf("Sum of %d digit is %d",no,sum);
+//   
+
+
+
+int c = 10+ 20*4-3+2/6-8+3;
+printf("%d",c);
+}
