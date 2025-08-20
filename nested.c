b70b5321f8a5ebf5bcc31c4b3a9c8ffa7bf46c5b
Adinathtambe <adinathtambe4@gmail.com>
2025-08-20 22:43:14 +0530

    nested statement

---
+++ nested.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+	int signal;
+	printf("Enter value of signal(1.red,2.yellow,3.green)\n");
+	scanf("%d",&signal);
+	
+	if(signal==1){
+		printf("stop");
+		}else if(signal==2){
+			printf("slowdown");
+			
+			
+		}else{
+		
+		printf("green");
+	}
+	}
+
